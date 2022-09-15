ca6821d78fd8df06ce53c1c557425d5ec6eecbb0
GDSDC <gdc.business.2019@gmail.com>
2022-09-15 16:42:44 +0200

    feat(database)

--- epic/epic/settings.py
+++ epic/epic/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'epic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'epic_crm',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
