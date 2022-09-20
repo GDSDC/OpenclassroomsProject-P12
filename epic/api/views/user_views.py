07f6e31bac818f1eb34e9755541614346a1c713f
GDSDC <gdc.business.2019@gmail.com>
2022-09-20 22:42:30 +0200

    refactor(user_views) : using isAdmin custom permission

--- epic/api/views/user_views.py
+++ epic/api/views/user_views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from api.serializers import LoginSerializer, SignUpSerializer
+from api.permissions import isAdmin
 
 
 class LoginView(APIView):
@@ -28,7 +29,6 @@ class LogoutView(APIView):
 
     # This view should be accessible only for IsAuthenticated users.
     permission_classes = (IsAuthenticated,)
-    serializer_class = LoginSerializer
 
     def get(self, request):
         user = request.user
@@ -39,18 +39,24 @@ class LogoutView(APIView):
 class SignupView(APIView):
     """Signup View"""
 
-    # This view should be accessible only for IsAuthenticated users and Admin users (user.role = 'ADMIN').
-    permission_classes = (IsAuthenticated,)
+    # This view should be accessible only for Authenticated Admin users.
+    permission_classes = (IsAuthenticated, isAdmin)
     serializer_class = SignUpSerializer
 
     def post(self, request):
-        # check if user is admin
-        user = request.user
-        if user.role != 'ADMIN':
-            return Response('Access Forbidden !', status=status.HTTP_403_FORBIDDEN)
-
         user_to_create = request.data
         serializer = self.serializer_class(data=user_to_create)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class DeleteUserView(APIView):
+    """Delete User View"""
+
+    # This view should be accessible only for IsAuthenticated users and Admin users (user.role = 'ADMIN').
+    permission_classes = (IsAuthenticated, isAdmin)
+
+    def delete(self, request, user_id):
+        pass
+        # TODO : develop this part
