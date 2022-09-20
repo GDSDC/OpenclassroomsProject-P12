cb9bc9c347c3cc721a3d5e6ef696eb0f9be6735a
GDSDC <gdc.business.2019@gmail.com>
2022-09-20 22:41:27 +0200

    feat(permissions) : creating custom permissions  file

---
+++ epic/api/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+from core.users.models import User
+
+
+class isAdmin(BasePermission):
+    """Permission Class for Admin users"""
+
+    def has_permission(self, request, view):
+        return request.user.role == User.Role.ADMIN.value
