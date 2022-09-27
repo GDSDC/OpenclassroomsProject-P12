0c52d5b8d6d5512334adac92789fb4028a8827aa
Antoine Latrille <antoine.latrille@gmail.com>
2022-09-27 13:00:54 +0200

    feat(decoratos): ajout du decorateur de verification de role

--- epic/api/views/contracts_views.py
+++ epic/api/views/contracts_views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from api.decorators import user_has_role
 from api.serializers import ContractSerializer
 from core.users.models import User
 from core.contacts.models import Contact
@@ -24,6 +25,7 @@ class GlobalContractsView(APIView):
         contracts = Contract.objects.all()
         return JsonResponse(self.serializer_class(contracts, many=True).data, status=status.HTTP_200_OK, safe=False)
 
+    @user_has_role(roles_in={User.Role.SALES, User.Role.ADMIN})
     def post(self, request, contact_id):
         # check if contact exists
         if not contact_exists(contact_id):
@@ -33,9 +35,8 @@ class GlobalContractsView(APIView):
 
         # check if user is owner of contact (ie is sales of the contact) or if is admin
         user = request.user
-        if not (user.role == User.Role.ADMIN or (
-                user.role == User.Role.SALES and Contact.objects.get(id=contact_id).sales_id == user.id)):
-            return Response('Access forbidden ! You are not attached to the contact or admin.',
+        if user.role == User.Role.SALES and not Contact.objects.get(id=contact_id).sales_id == user.id:
+            return Response('Access forbidden ! You are not attached to the contact.',
                             status=status.HTTP_403_FORBIDDEN)
 
         serializer = self.serializer_class(data=contract_to_create, context={'contact_id': contact_id}, partial=True)
