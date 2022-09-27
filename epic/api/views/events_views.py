c5d52c117fb3ea4eae5ae4f881acc4114089b233
GDSDC <gdc.business.2019@gmail.com>
2022-09-27 17:28:30 +0200

    fix(events) : EventView put

--- epic/api/views/events_views.py
+++ epic/api/views/events_views.py
@@ -73,7 +73,7 @@ class EventView(APIView):
 
         return JsonResponse(self.serializer_class(event).data, status=status.HTTP_200_OK, safe=False)
 
-    @user_has_role({User.Role.ADMIN, User.Role.SALES})
+    @user_has_role({User.Role.ADMIN, User.Role.SALES, User.Role.SUPPORT})
     def put(self, request, contact_id, event_id):
         # check if contact exists
         if not contact_exists(contact_id):
@@ -96,6 +96,11 @@ class EventView(APIView):
         contact = Contact.objects.get(id=contact_id)
         if user.role == User.Role.SALES and not contact.sales_id == user.id:
             return Response('Access forbidden ! You are not attached to the contact or admin.',
+                            status=status.HTTP_403_FORBIDDEN)
+
+        # check if user is attached to event (ie is support of the event)
+        if user.role == User.Role.SUPPORT and not event_to_update.support_id == user.id:
+            return Response('Access forbidden ! You are not attached to the event or admin.',
                             status=status.HTTP_403_FORBIDDEN)
 
         serializer = self.serializer_class(event_to_update, data=event_updated_data, partial=True)
