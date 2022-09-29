2521a21d3e4d7973894ffb4ef4183a0b7bc9a8fc
GDSDC <gdc.business.2019@gmail.com>
2022-09-29 18:19:49 +0200

    fix(users) : ADMIN role when createsuperuser

--- epic/core/users/models.py
+++ epic/core/users/models.py
@@ -32,6 +32,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', User.Role.ADMIN)
         return self._create_user(email, password, **extra_fields)
 
 
