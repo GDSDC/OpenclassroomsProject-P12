4eb4dc3d7633174c61e2289726119b0b81d28599
GDSDC <gdc.business.2019@gmail.com>
2022-10-03 17:24:30 +0200

    feat(logging) : creating logging for Event views

--- epic/api/views/events_views.py
+++ epic/api/views/events_views.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Any, Dict
 
 from django.http import JsonResponse
@@ -7,7 +8,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from api.serializers import EventSerializer
-from api.decorators import user_has_role, query_parameter_parser
+from api.decorators import user_has_role, query_parameter_parser, logging_and_response
 from core.users.models import User
 from core.contacts.models import Contact
 from core.contacts.services import contact_exists
@@ -33,9 +34,14 @@ class GlobalEventView(APIView):
 
     @user_has_role({User.Role.ADMIN, User.Role.SALES})
     def post(self, request, contact_id):
+        logger = logging.getLogger('.'.join([__name__, self.__class__.__name__, self.post.__name__]))
+
         # check if contact exists
         if not contact_exists(contact_id):
-            return Response('Contact not found. Wrong contact_id.', status=status.HTTP_404_NOT_FOUND)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Contact '{contact_id}' not found. Wrong contact_id.",
+                error_status=status.HTTP_404_NOT_FOUND)
 
         event_to_create = request.data
         contact = Contact.objects.get(id=contact_id)
@@ -43,6 +49,10 @@ class GlobalEventView(APIView):
         # check if user is owner of contact (ie is sales of the contact) or if is admin
         user = request.user
         if user.role == User.Role.SALES and not contact.sales_id == user.id:
+            logger.warning(
+                f"Access forbidden ! User '{user.email}' is not attached to contact '{contact_id}' "
+                f"({contact.first_name} {contact.last_name} "
+                f"from {contact.company_name} company) or admin.")
             return Response('Access forbidden ! You are not attached to the contact or admin.',
                             status=status.HTTP_403_FORBIDDEN)
 
@@ -59,50 +69,76 @@ class EventView(APIView):
     serializer_class = EventSerializer
 
     def get(self, request, contact_id, event_id):
+        logger = logging.getLogger('.'.join([__name__, self.__class__.__name__, self.get.__name__]))
+
         # check if contact exists
         if not contact_exists(contact_id):
-            return Response('Contact not found. Wrong contact_id.', status=status.HTTP_404_NOT_FOUND)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Contact '{contact_id}' not found. Wrong contact_id.",
+                error_status=status.HTTP_404_NOT_FOUND)
 
         # check if event exists
         if not event_exists(event_id):
-            return Response('Event not found. Wrong event_id.', status=status.HTTP_404_NOT_FOUND)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Event '{event_id}' not found. Wrong event_id.",
+                error_status=status.HTTP_404_NOT_FOUND)
 
         event = Event.objects.get(id=event_id)
 
         # check if event belongs to contact
         if not event.client.id == contact_id:
-            return Response(f"Event '{event_id}' does not belong to Client '{contact_id}' !",
-                            status=status.HTTP_400_BAD_REQUEST)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Event '{event_id}' does not belong to Client '{contact_id}' !",
+                error_status=status.HTTP_400_BAD_REQUEST)
 
         return JsonResponse(self.serializer_class(event).data, status=status.HTTP_200_OK, safe=False)
 
     @user_has_role({User.Role.ADMIN, User.Role.SALES, User.Role.SUPPORT})
     def put(self, request, contact_id, event_id):
+        logger = logging.getLogger('.'.join([__name__, self.__class__.__name__, self.put.__name__]))
+
         # check if contact exists
         if not contact_exists(contact_id):
-            return Response('Contact not found. Wrong contact_id.', status=status.HTTP_404_NOT_FOUND)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Contact '{contact_id}' not found. Wrong contact_id.",
+                error_status=status.HTTP_404_NOT_FOUND)
 
         # check if event exists
         if not event_exists(event_id):
-            return Response('Event not found. Wrong event_id.', status=status.HTTP_404_NOT_FOUND)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Event '{event_id}' not found. Wrong event_id.",
+                error_status=status.HTTP_404_NOT_FOUND)
 
         event_updated_data = request.data
         event_to_update = Event.objects.get(id=event_id)
 
         # check if event belongs to contact
         if not event_to_update.client.id == contact_id:
-            return Response(f"Event '{event_id}' does not belong to Client '{contact_id}' !",
-                            status=status.HTTP_400_BAD_REQUEST)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Event '{event_id}' does not belong to Client '{contact_id}' !",
+                error_status=status.HTTP_400_BAD_REQUEST)
 
         # check if user is owner of contact (ie is sales of the contact)
         user = request.user
         contact = Contact.objects.get(id=contact_id)
         if user.role == User.Role.SALES and not contact.sales_id == user.id:
+            logger.warning(
+                f"Access forbidden ! User '{user.email}' is not attached to contact '{contact_id}' "
+                f"({contact.first_name} {contact.last_name} "
+                f"from {contact.company_name} company) or admin.")
             return Response('Access forbidden ! You are not attached to the contact or admin.',
                             status=status.HTTP_403_FORBIDDEN)
 
         # check if user is attached to event (ie is support of the event)
         if user.role == User.Role.SUPPORT and not event_to_update.support_id == user.id:
+            logger.warning(
+                f"Access forbidden ! User '{user.email}' is not attached to event '{event_id}' or admin.")
             return Response('Access forbidden ! You are not attached to the event or admin.',
                             status=status.HTTP_403_FORBIDDEN)
 
@@ -113,20 +149,30 @@ class EventView(APIView):
 
     @user_has_role({User.Role.ADMIN})
     def delete(self, request, contact_id, event_id):
+        logger = logging.getLogger('.'.join([__name__, self.__class__.__name__, self.delete.__name__]))
+
         # check if contact exists
         if not contact_exists(contact_id):
-            return Response('Contact not found. Wrong contact_id.', status=status.HTTP_404_NOT_FOUND)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Contact '{contact_id}' not found. Wrong contact_id.",
+                error_status=status.HTTP_404_NOT_FOUND)
 
         # check if event exists
         if not event_exists(event_id):
-            return Response('Event not found. Wrong event_id.', status=status.HTTP_404_NOT_FOUND)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Event '{event_id}' not found. Wrong event_id.",
+                error_status=status.HTTP_404_NOT_FOUND)
 
         event_to_delete = Event.objects.get(id=event_id)
 
         # check if event belongs to contact
         if not event_to_delete.client.id == contact_id:
-            return Response(f"Event '{event_id}' does not belong to Client '{contact_id}' !",
-                            status=status.HTTP_400_BAD_REQUEST)
+            return logging_and_response(
+                logger=logger,
+                error_message=f"Event '{event_id}' does not belong to Client '{contact_id}' !",
+                error_status=status.HTTP_400_BAD_REQUEST)
 
         event_to_delete.delete()
         return Response('Event deleted successfully !', status=status.HTTP_200_OK)
