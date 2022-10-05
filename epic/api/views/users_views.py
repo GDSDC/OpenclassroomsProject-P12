e8c32190b9f373a2b0cecc98ab93621bfdb65b8a
GDSDC <gdc.business.2019@gmail.com>
2022-10-05 11:36:45 +0200

    refactor(users) : using IsAdminUser permission class instead of @user_has_role({User.Role.ADMIN})

--- epic/api/views/users_views.py
+++ epic/api/views/users_views.py
@@ -3,13 +3,13 @@ from typing import Any, Dict
 
 from django.contrib.auth import login, logout
 from django.http import JsonResponse
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 from api.serializers import LoginSerializer, SignUpSerializer, UserSerializer
-from api.decorators import user_has_role, logging_and_response, query_parameter_parser
+from api.decorators import logging_and_response, query_parameter_parser
 from core.users.models import User
 from core.users.services import user_exists
 
@@ -46,10 +46,9 @@ class SignupView(APIView):
     """Signup View"""
 
     # This view should be accessible only for Authenticated Admin users.
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsAdminUser)
     serializer_class = SignUpSerializer
 
-    @user_has_role({User.Role.ADMIN})
     def post(self, request):
         user_to_create = request.data
         serializer = self.serializer_class(data=user_to_create)
@@ -62,10 +61,9 @@ class ManagerUserView(APIView):
     """View or Delete a User View"""
 
     # This view should be accessible only for IsAuthenticated users and Admin users (user.role = 'ADMIN').
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsAdminUser)
     serializer_class = UserSerializer
 
-    @user_has_role({User.Role.ADMIN})
     @query_parameter_parser({'role': User.Role})
     def get(self, request, query_params: Dict[str, Any], user_id=None):
         # Looking for a unique User data
@@ -84,7 +82,6 @@ class ManagerUserView(APIView):
             is_many = True
         return JsonResponse(self.serializer_class(users, many=is_many).data, status=status.HTTP_200_OK, safe=False)
 
-    @user_has_role({User.Role.ADMIN})
     def delete(self, request, user_id):
         # check if user_to_delete exists
         if not user_exists(user_id=user_id):
